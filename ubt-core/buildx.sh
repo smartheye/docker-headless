f69e0885245fb5f0d40fd39b2a23ac4379b6395e
Andrew Ye HE <heyehy@cn.ibm.com>
2024-03-15 16:54:02 +0800

    test

--- ubt-core/buildx.sh
+++ ubt-core/buildx.sh
@@ -3,14 +3,20 @@
 
 source /etc/profile
 export |grep DOCKER_REG
-repo=registry.cn-hangzhou.aliyuncs.com
-echo "${DOCKER_REGISTRY_PW_aliyun}" |docker login --username=${DOCKER_REGISTRY_USER_aliyun} --password-stdin $repo
+repoAliyun=registry.cn-hangzhou.aliyuncs.com
+echo "${DOCKER_REGISTRY_PW_aliyun}" |docker login --username=${DOCKER_REGISTRY_USER_aliyun} --password-stdin $repoAliyun
 
 repoHub=docker.io
 echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTRY_USER_dockerhub} --password-stdin $repoHub
 
-ns=docker-headless
+repo=registry-1.docker.io
+ns=smartheye
 ver=v61 #base-v5 base-v5-slim
+
+# docker build commandline
+#push="--push"
+push=""
+
 case "$1" in
 compile)
     # TigerVNC 1.12.0 |10 Nov 2021
@@ -24,16 +30,14 @@ compile)
     # curl -O -fsSL https://www.linuxfromscratch.org/patches/blfs/svn/tigervnc-1.12.0-configuration_fixes-1.patch
     cd $old;
     #
-    repo=registry-1.docker.io
     img="docker-headless:core-compile"
     plat="--platform linux/amd64,linux/arm64" #,linux/arm
     # --network=host: docker buildx create --use --name mybuilder2 --buildkitd-flags '--allow-insecure-entitlement network.host'
-    docker buildx build $plat --push -t $ns/$img -f src/Dockerfile.compile .
+    docker buildx build $plat $push -t $ns/$img -f src/Dockerfile.compile .
     ;;
 
 # +slim,base (docker-headless,weskit; use docker-headless's)
 slim)
-    repo=registry-1.docker.io
     # repo=registry.cn-hangzhou.aliyuncs.com #arm64/amd64可以用,只是cr.console.aliyun.com上无多镜像显示
     img="docker-headless:base-$ver-slim"
     # cache
@@ -46,7 +50,6 @@ slim)
     docker buildx build $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.base .
     ;;
 base)
-    repo=registry-1.docker.io
     img="docker-headless:base-$ver"
     # cache
     ali="registry.cn-hangzhou.aliyuncs.com"
@@ -57,8 +60,10 @@ base)
     #plat="--platform linux/amd64,linux/arm64,linux/arm"
     plat="--platform linux/amd64"
     args="--build-arg FULL=/.."
-    echo "docker buildx build --progress=plain $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.base ."
-    docker buildx build --progress=plain $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.base .
+    #echo "docker buildx build --progress=plain $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.base ."
+    #docker buildx build --progress=plain $cache $plat $args --push -t $repo/$ns/$img -f src/Dockerfile.base .
+    echo "docker buildx build --progress=plain $cache $plat $args $push -t $repo/$ns/$img -f src/Dockerfile.base ."
+    docker buildx build --progress=plain $cache $plat $args $push -t $repo/$ns/$img -f src/Dockerfile.base .
     ;;
 *)
     repo=registry-1.docker.io
